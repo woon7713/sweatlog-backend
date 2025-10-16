f06845e6d90c91db3bce5ad072de361a237637eb
woon <woon7713@gmail.com>
2025-10-16 17:15:06 +0900

    feat: inital Spring Boot BE

---
+++ src/main/java/com/w7/sweatlog_backend/SweatlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.w7.sweatlog_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweatlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweatlogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/w7/sweatlog_backend/SweatlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w7.sweatlog_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweatlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
